fc39ac3f2e27d2ec6d68374c3efcf90ff752ac9b
Sushanthi Charukonda <chsushanthi@yahoo.com>
2017-08-16 04:41:15 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var pug = require("gulp-pug");
+var less = require("gulp-less");
+var watch= require("gulp-watch");
+var minify= require("gulp-minify");
+var prettify = require("gulp-prettify");
+var gulp=require("gulp");
+var browsersync = require('browser-sync');
+var runsequence= require('run-sequence');
+
+//create our first task.Each task has a name and a function attached to it
+
+gulp.task('pug',function(){
+    //alll pug files will be in templates folder with .pug extension
+    return gulp.src('templates/*.pug')
+   .pipe(pug())
+    .pipe(prettify())
+    .pipe(gulp.dest('build'))
+    .pipe(browsersync.reload({
+        stream:true
+    }))
+    
+});
+
+//for css we are going to create a new task in  a similar pattern
+gulp.task('less',function(){
+    
+    //store less files in less folder
+    return gulp.src('less/*.less')
+    
+    .pipe(less())
+    .pipe(minify())
+    .pipe(gulp.dest('build'))
+    .pipe(browsersync.reload({
+        stream:true
+    }))
+});
+
+//another task for our java script
+gulp.task('js',function(){
+    
+    //store java script files in scripts folder
+    return gulp.src('scripts/*.js')
+    .pipe(minify())
+    .pipe(gulp.dest('build'))
+    .pipe(browsersync.reload({
+        stream:true
+    }))
+});
+
+
+gulp.task('browsersync',function() {
+    browsersync.init({server: {baseDir: 'build'},
+    port: '8082'
+    });
+});
+
+//create task for watch
+gulp.task('watch',function(){
+    runsequence('pug','less','js','browsersync',function(e){
+       //not yey anything 
+    });
+    
+    //semicolons are not mandatory in java script
+    
+    //runs the watch module on the folders and runs the corresponding tasks
+    gulp.watch('templates/*.pug' ,['pug']);
+    gulp.watch('less/*.less' ,['less']);
+    gulp.watch('scripts/*.js', ['js']);
+    
+});
+
+
+
+
---
+++ scripts/main.js
@@ -0,0 +1,133 @@
+//define task class
+//task has a name,status and id
+class Task {
+    constructor(taskname){
+        this.name = taskname;
+        this.status = 0;
+        //time stamp
+        this.id = new Date().getTime();
+        //this returns an object of itself
+        return this;
+    }
+}
+
+//array to store tasks
+//let prevents global accesibility whereas var is global
+var TaskArray = [];
+
+window.addEventListener('load',function(){
+    loadTasks();
+    //create  a reference for  a form using  a constant
+    const TaskForm=document.getElementById('task-form');
+    
+    //make this form listen to the event
+    //add a listener when form is submitted
+    TaskForm.addEventListener('submit',onSubmit);
+    //get  areference to task-list element
+    const TaskList = document.getElementById('task-list');
+    TaskList.addEventListener('click',changeTaskStatus);
+});
+
+
+
+//define onSubmit
+function onSubmit(event){
+    //cancel event default
+    event.preventDefault();
+    const TaskInput = document.getElementById('task-input');
+    let taskname = TaskInput.value;
+    if(taskname != ''){
+        let todo = new Task(taskname);
+        TaskArray.push(todo);
+        saveTasks();
+        //createNewTask(todo);
+        event.target.reset();
+        renderTaskList();
+    }
+     //if we want the action in console
+  //  console.log(taskname);
+ //   console.log(console);
+  
+}
+  //to convert this task inti html element
+  function createNewTask(taskobj){
+     
+      //create an LI element
+      let listitem = document.createElement('LI');
+      //set attributes to the LI
+      listitem.setAttribute('id',taskobj.id);
+      listitem.setAttribute('data-status',taskobj.status);
+      let txt = document.createTextNode(taskobj.name);
+      listitem.appendChild(txt);
+      //add this to task list
+      document.getElementById('task-list').appendChild(listitem);
+  }
+  
+  function clearTaskList(){
+      //innerHTML holds all html tags from task list
+      document.getElementById('task-list').innerHTML = '';
+  }
+  
+  function renderTaskList(){
+      let count = TaskArray.length;
+      clearTaskList();
+      for(let i=0;i < count;i++){
+          let taskobj = TaskArray[i];
+          createNewTask(taskobj);
+      }
+  }
+  
+  //will see the event from the parent instead of the item on which the event is occuring 
+  function changeTaskStatus(event){
+      console.log(event.target);
+      let itemid = event.target.id;
+      //search the array for the itemid
+      let count = TaskArray.length;
+      //loop through array
+      for(let i=0;i < count; i++){
+          let taskobj = TaskArray[i];
+          if (taskobj.id == itemid){
+              switch (taskobj.status) {
+                  case 0:
+                      taskobj.status = 1;
+                      break;
+                   case 1:
+                      taskobj.status = 0;
+                      break;   
+                   default:
+                      break;
+              }
+              saveTasks();
+              renderTaskList();
+          }
+      }
+      
+  }
+  
+  //
+  function saveTasks(){
+      if(window.localStorage){
+      //converting into string
+          let jsonstring = JSON.stringify(TaskArray);
+          window.localStorage.setItem('tasks',jsonstring);
+      }
+  } 
+
+function loadTasks(){
+    if(window.localStorage){
+        let data = window.localStorage.getItem('tasks');
+        //let jsondata = JSON.parse(data);
+        if(JSON.parse(data)){
+            TaskArray = JSON.parse(data);
+            renderTaskList();
+        }
+        //console.log(jsondata);
+        //convert that data into an array
+       // TaskArray = new Array(JSON.parse(data));
+      // TaskArray = new Array(jsondata);
+     // if(jsondata.length > 0){
+      //TaskArray = jsondata;
+       //renderTaskList();
+      //}
+    }
+}
